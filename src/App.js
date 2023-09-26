a770b4ccf59693f973b48c22a937c065a3f7a9d4
DRAKONNN <rafakhis14@gmail.com>
2023-09-26 12:34:36 +0200

    Programming navbar renders

--- src/App.js
+++ src/App.js
@@ -4,9 +4,18 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
+import React, { Component, useState } from 'react'
+
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Placeholder from 'react-bootstrap/Placeholder';
+
+
+import Stories from './components/stories';
+import Characters from './components/characters';
 
 function App() {
   const backgroundStyle = {
@@ -30,10 +39,33 @@ function App() {
     left: 0,
     zIndex: -1,
   };
+
+  const [activeTab, setActiveTab] = useState('stories');
+
+  const renderContent = () => {
+    if (activeTab === 'stories') {
+      return <Stories />;
+    } else if (activeTab === 'characters') {
+      return <Characters />;
+    }
+  };
   return (
     <div style={backgroundStyle}>
       <div style={blurLayerStyle}></div>
 
+      <Nav  className="justify-content-center navbar sticky-top">
+        <Nav.Item>
+          <Nav.Link eventKey="link-1" className="glow text-white" onClick={() => setActiveTab('stories')}>Historias</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2" className="glow text-white">MYSTERY-TALES</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2" className="glow text-white" onClick={() => setActiveTab('characters')}>Personajes</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      
+      {/* =====DEPRECATED=====
       <nav className="navbar navbar-expand-lg bg-dark navbar-dark sticky-top">
         <div className="container-fluid">
           <a className="navbar-brand glow" href="#">MYSTERY-TALES</a>
@@ -41,53 +73,26 @@ function App() {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="collapseNavbar">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0 glow">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 glow text-center">
+              <li className={`nav-item ${activeTab === 'stories' ? 'active' : ''}`}>
+                <button className="nav-link glow" onClick={() => setActiveTab('stories')}>
+                  Historias
+                </button>
+              </li>
+              <li className={`nav-item ${activeTab === 'characters' ? 'active' : ''}`}>
+                <button className="nav-link glow" onClick={() => setActiveTab('characters')}>
+                  Personajes
+                </button>
+              </li>
             </ul>
           </div>
         </div>
-      </nav>
+      </nav>*/}
 
       <div className="container">
-        <section className="resume-section" id="proyectos">
-          <div className="resume-section-content">
-            <h1 className="">Historias</h1>
-            <div id="row-stories" class="row">
-              <div className="col-lg-4">
-                <Card style={{ width: '18rem', margin: '5rem' }}>
-                  <Card.Img variant="top" src="holder.js/100px180" />
-                  <Card.Body>
-                    <Placeholder as={Card.Title} animation="glow">
-                      <Placeholder xs={6} />
-                    </Placeholder>
-                    <Placeholder as={Card.Text} animation="glow">
-                      <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{' '}
-                      <Placeholder xs={6} /> <Placeholder xs={8} />
-                    </Placeholder>
-                    <Placeholder.Button variant="primary" xs={6} />
-                  </Card.Body>
-                </Card>
-              </div>
-              <div className="col-lg-4">
-                <Card style={{ width: '18rem', margin: '5rem' }}>
-                  <Card.Img variant="top" src="holder.js/100px180" />
-                  <Card.Body>
-                    <Placeholder as={Card.Title} animation="glow">
-                      <Placeholder xs={6} />
-                    </Placeholder>
-                    <Placeholder as={Card.Text} animation="glow">
-                      <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{' '}
-                      <Placeholder xs={6} /> <Placeholder xs={8} />
-                    </Placeholder>
-                    <Placeholder.Button variant="primary" xs={6} />
-                  </Card.Body>
-                </Card>
-              </div>
-            </div>
-          </div>
-        </section>
+        <div className="container mt-4">
+          {renderContent()}
+        </div>
       </div>
     </div>
   );
---
+++ src/components/characters.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Placeholder from 'react-bootstrap/Placeholder';
+
+function Characters() {
+  return (
+    <div className="resume-section-content">
+      <h1 className="">Personajes</h1>
+      <div id="row-stories" class="row">
+        <div className="col-lg-4">
+          <Card style={{ width: '18rem', margin: '5rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Placeholder as={Card.Title} animation="glow">
+                <Placeholder xs={6} />
+              </Placeholder>
+              <Placeholder as={Card.Text} animation="glow">
+                <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{' '}
+                <Placeholder xs={6} /> <Placeholder xs={8} />
+              </Placeholder>
+              <Placeholder.Button variant="primary" xs={6} />
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Characters;
