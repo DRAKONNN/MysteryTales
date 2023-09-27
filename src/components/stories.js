7aac9fdfd0d1f67cb192762d014ed05b427cd1af
DRAKONNN <rafakhis14@gmail.com>
2023-09-27 14:51:49 +0200

    Introducing story placeholders

--- src/components/stories.js
+++ src/components/stories.js
@@ -12,11 +12,23 @@ function Stories() {
     <div className="resume-section-content">
       <h1 className="">Historias</h1>
       <div className="row gy-5">
-        <div className="col-lg-4">
-          
+        <div className="col-lg-3">
+          <div className="card bg-dark text-white">
+            <div className="card-img-wrapper">
+              <img src="/images/DurakAI.png" className="card-img zoom-effect" alt="..." />
+            </div>
+            <div className="card-img-overlay d-flex justify-content-center align-items-center">
+              <h3 className="card-title">Kremlin</h3>
+            </div>
+          </div>
         </div>
-        <div className="col-lg-4">
-          
+        <div className="col-lg-3">
+          <div class="card bg-dark text-white">
+            <img src="/images/MentirosoAI.png" class="card-img" alt="..." />
+            <div className="card-img-overlay d-flex justify-content-center align-items-center">
+              <h3 className="card-title">Western</h3>
+            </div>
+          </div>
         </div>
       </div>
     </div>
