ef193069520fd5d0fffa1adfe1ce379c5973266a
DRAKONNN <rafakhis14@gmail.com>
2023-10-03 15:13:09 +0200

    Developing puzzle modals

--- src/components/puzzles.js
+++ src/components/puzzles.js
@@ -4,9 +4,9 @@ import { useState, useCallback, useEffect, useRef } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
+import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import Placeholder from 'react-bootstrap/Placeholder';
 
 import puzzles from '../data/datapuzzles';
 import Footer from '../components/footer';
@@ -15,13 +15,19 @@ const PUZZLES = puzzles;
 
 function Puzzle(props) {
   const {puzzle} = props
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div className="col-lg-4">
-        <div className={`card shadow-box bg-dark`}>
+      <div className={`card shadow-box bg-dark`}>
         <div class="card-body text-white">
           <h5 class="card-title text-warning">{puzzle.title}</h5>
           <p class="card-text">{puzzle.description}</p>
-          <button type="button" class="btn btn-warning text-white">Leer</button>
+          <button type="button" class="btn btn-warning text-white" onClick={handleShow}>Leer</button>
         </div>
         <div className="img-zoom">
           <a href={puzzle.url}>
@@ -29,6 +35,30 @@ function Puzzle(props) {
           </a>
         </div>
       </div>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header className="bg-dark" closeButton>
+          <Modal.Title className='text-warning'>{puzzle.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="bg-dark text-white">
+          <p dangerouslySetInnerHTML={{ __html: puzzle.question }} />
+          <p>
+            <button class="btn btn-warning text-white" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+              Revelar la solución
+            </button>
+          </p>
+          <div class="collapse" id="collapseExample">
+            <div class="card card-body bg-dark bg-gradient">
+              <p dangerouslySetInnerHTML={{ __html: puzzle.solution }} />
+            </div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer className="bg-dark">
+          <Button variant="secondary" onClick={handleClose}>
+            Cerrar
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   )
 }
--- src/data/datapuzzles.js
+++ src/data/datapuzzles.js
@@ -2,6 +2,16 @@ export default [{
   id: 'p01', 
   title: "El acertijo de la bombilla",
   description: 'Una bombilla y tres interruptores. ¿Qué interruptor enciende la bombilla?',
+  question: 'Tienes una habitación con una puerta cerrada. En el interior de la habitación, hay una bombilla que está apagada y que no puedes ver desde fuera. También tienes tres interruptores en la pared fuera de la habitación. Estos interruptores están etiquetados como interruptor A, interruptor B y interruptor C, pero no sabes qué interruptor controla la bombilla.<br>' +
+    "Solo puedes entrar en la habitación una vez.<br>" +
+    "¿Cómo puedes determinar cuál de los tres interruptores controla la bombilla si solo puedes entrar en la habitación una vez?",
+  solution: "1. Enciende el interruptor A y déjalo encendido durante unos minutos.<br>" +
+    "2. Luego, apaga el interruptor A y enciende el interruptor B.<br>" +
+    "3. Inmediatamente entra en la habitación.<br>" +
+    "Ahora, aquí está lo que sucede:<br>" +
+    "- Si la bombilla está encendida cuando entras en la habitación, entonces el interruptor B controla la bombilla.<br>" +
+    "- Si la bombilla está apagada pero caliente al tacto, entonces el interruptor A controla la bombilla (ya que la encendiste previamente y se calentó antes de apagarlo).<br>" +
+    '- Si la bombilla está apagada y fría al tacto, entonces el interruptor C controla la bombilla, ya que nunca la encendiste.',
   image: '/images/puzzles/Bombilla.jpg',
   classAttText: 'text-white',
 }];
