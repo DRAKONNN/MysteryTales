b47339e75ab65f74afad510f6fa35b27ff99a867
DRAKONNN <rafakhis14@gmail.com>
2023-10-05 21:33:28 +0200

    Introducing elevator puzzle

--- src/data/datapuzzles.js
+++ src/data/datapuzzles.js
@@ -15,4 +15,15 @@ export default [{
     '- Si la bombilla está apagada y fría al tacto, entonces el interruptor C controla la bombilla, ya que nunca la encendiste.',
   image: '/images/puzzles/Puzzle_Bombilla.png',
   classAttText: 'text-white',
+}, {
+  id: 'p01', 
+  title: "El acertijo del ascensor",
+  description: 'Un hombre misteriosamente se baja del ascensor en diferentes pisos.',
+  difficulty: 'baja',
+  question: "Un hombre vive en el piso 15 de un edificio. Todos los días, cuando va al trabajo, toma el ascensor desde su piso hasta la planta baja.<br>" + 
+    "Cuando regresa del trabajo, generalmente toma el ascensor hasta el piso 7 y luego sube los 8 pisos restantes por las escaleras. Sin embargo, cuando llueve, toma el ascensor directamente hasta su piso en el piso 15. ¿Por qué hace esto?",
+  solution: "El hombre es de estatura baja y no alcanza a presionar el botón del piso 15 del ascensor, excepto cuando lleva un paraguas.<br>" + 
+    "Cuando llueve y lleva un paraguas, puede usar el mango del paraguas para presionar el botón del piso 15. Cuando no llueve, no puede alcanzar ese botón y, por lo tanto, solo sube hasta el séptimo piso antes de tomar las escaleras para llegar a su piso en el 15",
+  image: '/images/puzzles/Puzzle_Ascensor.png',
+  classAttText: 'text-white',
 }];
