b098a27b6343581df8491d3db6e391116b0bcc41
DRAKONNN <rafakhis14@gmail.com>
2023-10-10 17:11:05 +0200

    Developing pagination stories

--- src/components/stories.js
+++ src/components/stories.js
@@ -4,20 +4,51 @@ import { useState, useCallback, useEffect, useRef } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
+import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Placeholder from 'react-bootstrap/Placeholder';
 
 import stories from '../data/datastories';
+import books from '../data/databooks';
 import Footer from '../components/footer';
 
 const STORIES = stories;
+const BOOKS = books;
+
+function findBookByStoryId(storyId) {
+  return books.find(book => book.id === storyId);
+}
 
 function Story(props) {
   const {story} = props
+
+  const [show, setShow] = useState(false);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const book = findBookByStoryId(story.id);
+
+  const handleClose = () => setShow(false);
+  const handleShow = (pageIndex) => {
+    setCurrentPage(0);
+    setShow(true);
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < book.pages.length - 1) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
   return (
     <div className="col-lg-3">
-      <div className="card bg-dark text-white hover-zoom-interest">
+      <div className="card bg-dark text-white hover-zoom-interest" onClick={handleShow}>
         <div className="card-img-wrapper">
           <img src={story.image} className="card-img zoom-effect" alt={story.title} />
         </div>
@@ -25,6 +56,30 @@ function Story(props) {
           <h3 className={`card-title title-story text-center ${story.classAttText}`}>{story.title}</h3>
         </div>
       </div>
+      
+      <Modal show={show} size="lg" aria-labelledby="contained-modal-title-vcenter" centered onHide={handleClose} scrollable={true}>
+        <Modal.Header className="bg-dark" closeButton>
+          <Modal.Title className='text-warning'>{story.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="bg-dark text-white">
+          {book ? (
+            <p>{book.pages[currentPage]}</p>
+          ) : (
+            <p>No se encontró contenido para esta historia.</p>
+          )}
+        </Modal.Body>
+        <Modal.Footer className="bg-dark">
+          <Button variant="secondary" onClick={handlePrevPage} disabled={currentPage === 0}>
+            Anterior
+          </Button>
+          <Button variant="secondary" onClick={handleNextPage} disabled={currentPage === BOOKS[0].pages.length - 1}>
+            Siguiente
+          </Button>
+          <Button variant="secondary" onClick={handleClose}>
+            Cerrar
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   )
 }
---
+++ src/data/databooks.js
@@ -0,0 +1,68 @@
+export default [{
+  id: 'mansionsecret01',
+  pages: [
+    `This is some placeholder content to show the scrolling behavior for modals. 
+    We use repeated line breaks to demonstrate how content can exceed minimum inner height, thereby showing inner scrolling. 
+    When content becomes longer than the predefined max-height of modal, content will be cropped and scrollable within the modal.
+    This is some placeholder content to show the scrolling behavior for modals. 
+    We use repeated line breaks to demonstrate how content can exceed minimum inner height, thereby showing inner scrolling. 
+    When content becomes longer than the predefined max-height of modal, content will be cropped and scrollable within the modal.
+    This is some placeholder content to show the scrolling behavior for modals. 
+    We use repeated line breaks to demonstrate how content can exceed minimum inner height, thereby showing inner scrolling. 
+    When content becomes longer than the predefined max-height of modal, content will be cropped and scrollable within the modal.
+    This is some placeholder content to show the scrolling behavior for modals. 
+    We use repeated line breaks to demonstrate how content can exceed minimum inner height, thereby showing inner scrolling. 
+    When content becomes longer than the predefined max-height of modal, content will be cropped and scrollable within the modal.
+    This is some placeholder content to show the scrolling behavior for modals. 
+    We use repeated line breaks to demonstrate how content can exceed minimum inner height, thereby showing inner scrolling. 
+    When content becomes longer than the predefined max-height of modal, content will be cropped and scrollable within the modal.
+    This is some placeholder content to show the scrolling behavior for modals. 
+    We use repeated line breaks to demonstrate how content can exceed minimum inner height, thereby showing inner scrolling. 
+    When content becomes longer than the predefined max-height of modal, content will be cropped and scrollable within the modal.`,
+
+    `This is some placeholder content to show the scrolling behavior for modals. 
+    We use repeated line breaks to demonstrate how content can exceed minimum inner height, thereby showing inner scrolling. 
+    When content becomes longer than the predefined max-height of modal, content will be cropped and scrollable within the modal.
+    This is some placeholder content to show the scrolling behavior for modals. 
+    We use repeated line breaks to demonstrate how content can exceed minimum inner height, thereby showing inner scrolling. 
+    When content becomes longer than the predefined max-height of modal, content will be cropped and scrollable within the modal.`,
+  ]
+}, {
+  id: 'nautilus02',
+  pages: [
+    `This is some placeholder content to show the scrolling behavior for modals. 
+    We use repeated line breaks to demonstrate how content can exceed minimum inner height, thereby showing inner scrolling. 
+    When content becomes longer than the predefined max-height of modal, content will be cropped and scrollable within the modal.
+    This is some placeholder content to show the scrolling behavior for modals. 
+    We use repeated line breaks to demonstrate how content can exceed minimum inner height, thereby showing inner scrolling. 
+    When content becomes longer than the predefined max-height of modal, content will be cropped and scrollable within the modal.`,
+
+    `This is some placeholder content to show the scrolling behavior for modals. 
+    We use repeated line breaks to demonstrate how content can exceed minimum inner height, thereby showing inner scrolling. 
+    When content becomes longer than the predefined max-height of modal, content will be cropped and scrollable within the modal.
+    This is some placeholder content to show the scrolling behavior for modals. 
+    We use repeated line breaks to demonstrate how content can exceed minimum inner height, thereby showing inner scrolling. 
+    When content becomes longer than the predefined max-height of modal, content will be cropped and scrollable within the modal.`,
+  ]
+}, {
+  id: 'camplake03',
+  pages: [
+    `This is some placeholder content to show the scrolling behavior for modals. 
+    We use repeated line breaks to demonstrate how content can exceed minimum inner height, thereby showing inner scrolling. 
+    When content becomes longer than the predefined max-height of modal, content will be cropped and scrollable within the modal.
+    This is some placeholder content to show the scrolling behavior for modals. 
+    We use repeated line breaks to demonstrate how content can exceed minimum inner height, thereby showing inner scrolling. 
+    When content becomes longer than the predefined max-height of modal, content will be cropped and scrollable within the modal.`,
+
+    `This is some placeholder content to show the scrolling behavior for modals. 
+    We use repeated line breaks to demonstrate how content can exceed minimum inner height, thereby showing inner scrolling. 
+    When content becomes longer than the predefined max-height of modal, content will be cropped and scrollable within the modal.`,
+
+    `This is some placeholder content to show the scrolling behavior for modals. 
+    We use repeated line breaks to demonstrate how content can exceed minimum inner height, thereby showing inner scrolling. 
+    When content becomes longer than the predefined max-height of modal, content will be cropped and scrollable within the modal.
+    This is some placeholder content to show the scrolling behavior for modals. 
+    We use repeated line breaks to demonstrate how content can exceed minimum inner height, thereby showing inner scrolling. 
+    When content becomes longer than the predefined max-height of modal, content will be cropped and scrollable within the modal.`,
+  ]
+}];
